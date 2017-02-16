d86495a7c75d040f979d7d1430e928d007161df6
Vitaly Kezlya <vitaly@kezlya.com>
2017-02-16 15:23:16 -0500

    Initial Commit

---
+++ NYTimesChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NYTimesChallenge
+//
+//  Created by Chelsea Power on 2/16/17.
+//  Copyright © 2017 Brian Maci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

88b47e57d40d721705e47ef9bf7902ff8855d9ae
Vitaly Kezlya <briancmaci@gmail.com>
2017-02-18 00:40:50 -0500

    changed email user + BooksReadModel blank add

---
+++ NYTimesChallenge/model/BooksReadModel.swift
@@ -0,0 +1,13 @@
+//
+//  BooksReadModel.swift
+//  NYTimesChallenge
+//
+//  Created by Chelsea Power on 2/18/17.
+//  Copyright © 2017 Brian Maci. All rights reserved.
+//
+
+import UIKit
+
+class BooksReadModel: NSObject {
+
+}
